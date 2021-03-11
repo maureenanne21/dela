0b8f0762a5f7b9ecd4a9db5e1237d99bd9402381
maureen <maureenannw@gmail.com>
2021-03-11 13:33:14 +0300

    add my CSS/scripts.js,JS/scripts.js,index.tml

---
+++ JS/scripts.js
@@ -0,0 +1,23 @@
+function myFunction(){  //calling
+    var cc=document.getElementById("century");
+    var yy=parseInt(document.getElementById("year").value)
+    var mm=parseInt(document.getElementById("month").value)
+    var dd=parseInt(document.getElementById("day").value)
+    var male=document.getElementById("male");
+    var female=document.getElementById("female");     //stating arrays
+     var femaleNames=
+        ["Akosua",
+        "Adowa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Afua",
+        "Ama"]; 
+    var maleNames=
+       ["Kwasi",
+        "Kwadwo",
+        "Kwabena",
+        "Kwafu",
+        "Yaw",
+        "Kofi",
+        "Kwame",]
