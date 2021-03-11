b4f984f248009aada25e7829afafc7baef1c26cf
maureen <maureenannw@gmail.com>
2021-03-11 13:35:46 +0300

    add changes to my JS folder

--- JS/scripts.js
+++ JS/scripts.js
@@ -21,3 +21,22 @@ function myFunction(){  //calling
         "Yaw",
         "Kofi",
         "Kwame",]
+         //0       1        2          3           4         5           6
+    var weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friaday","Saturday"];    //validating
+    if(dd<=0||dd>31){
+        alert("invalid day please put the correct day!")
+    }
+    else if(mm<=0||mm>12){
+        alert("invalid year please put the correct month!")
+    }
+    if(yy<190||yy>2020){
+        alert("invalid year please put the correct year!")
+    }
+    else if(cc<19||cc>20){
+        alert("invalid century please put put the correct century")
+    }    var day =new Date(cc + "-" + yy + "-" + mm + "-" + dd);    var dayWanted=day.getDay();    if(male.checked==true){
+        document.getElementById("text").innerHTML=("You were born on " +weekDays[dayWanted]+ " and your akan name is " + maleNames[dayWanted])
+    }
+    else if(female.checked==true){
+        document.getElementById("text").innerHTML=("You were born on " +weekDays[dayWanted]+ " and your akan name is " + femaleNames[dayWanted])
+    }}
